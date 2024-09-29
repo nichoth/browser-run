9c66c898b5c217bc150fe2d27b9fbce32a011c6f
nichoth <nichoth@nichoth.com>
2024-09-28 22:54:16 -0700

    wip

--- test/static.js
+++ test/static.js
@@ -14,3 +14,16 @@ test('static', function (t, done) {
   browser.write('fetch("/static.js").then(res => res.text()).then(body => {console.log(body); window.close()})');
   browser.end();
 });
+
+test('JS module', (t, done) => {
+  const browser = run({ static: __dirname });
+
+  browser.on('data', function (data) {
+    // assert.strictEqual(data.trim(), fs.readFileSync(`${__dirname}/test.mjs`).toString().trim());
+    assert.ok(data.toString().includes('import'))
+    done();
+  });
+
+  browser.write('import { test } from "/test.mjs; window.close()');
+  browser.end();
+})
